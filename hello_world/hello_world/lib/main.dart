898a082802f0dc690d9590fbe84d83b9043c84f5
nownabe <nownabe@gmail.com>
2020-08-13 21:30:22 +0900

    Using Material Components

--- hello_world/hello_world/lib/main.dart
+++ hello_world/hello_world/lib/main.dart
@@ -1,63 +1,40 @@
 import 'package:flutter/material.dart';
 
-class MyAppBar extends StatelessWidget {
-  MyAppBar({this.title});
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Tutorial',
+    home: TutorialHome(),
+  ));
+}
 
-  final Widget title;
-
+class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.blue[500]),
-      child: Row(
-        children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null,
-          ),
-          Expanded(
-            child: title,
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: Text('AppBar Title'),
+        actions: <Widget>[
           IconButton(
             icon: Icon(Icons.search),
             tooltip: 'Search',
             onPressed: null,
           )
         ]
-      )
+      ),
+      body: Center(
+        child: Text('Hello, world!'),
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add',
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
     );
   }
-}
-
-class MyScaffold extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Column(
-        children: <Widget>[
-          MyAppBar(
-            title: Text(
-              "Example title",
-              style: Theme.of(context).primaryTextTheme.headline6,
-            ),
-          ),
-          Expanded(
-            child: Center(
-              child: Text('Hello, world!')
-            )
-          )
-        ]
-      )
-    );
-  }
-}
-
-void main() {
-  runApp(MaterialApp(
-    title: 'My app',
-    home: MyScaffold(),
-  ));
 }
